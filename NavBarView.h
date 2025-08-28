4031ff4742c4158ac380f761a0dade316feaa49e
mekabrine <119816853+mekabrine@users.noreply.github.com>
2025-08-27 23:06:11 -0700

    Delete NavBarView.h

--- NavBarView.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-@interface NavBarView : UIView
-@property (nonatomic, copy) void (^onHome)(void);
-@property (nonatomic, copy) void (^onBack)(void);
-@property (nonatomic, copy) void (^onRecents)(void);
-@end
